b164f44ec33d9c3b50589211b6d3a5cf1f753e22
Juandoqg <juandoqg@gmail.com>
2025-07-09 10:29:49 -0500

    admin component + ruta con guardAdmin

--- cine-frontend/src/app/app.routes.ts
+++ cine-frontend/src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { ProximamenteComponent } from './pages/proximamente/proximamente.component';
 import { DetallePeliculaComponent} from './pages/detalle-pelicula/detalle-pelicula.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'proximamente', component: ProximamenteComponent },
   { path: 'pelicula/:id', component: DetallePeliculaComponent},
-
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AdminGuard]
+  },
 
 
 
