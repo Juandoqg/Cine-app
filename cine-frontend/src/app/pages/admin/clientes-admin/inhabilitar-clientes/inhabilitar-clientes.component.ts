5204121acfc3b8ca168c779ec58cc17eccb36c6c
Juandoqg <juandoqg@gmail.com>
2025-07-11 14:56:26 -0500

    uso del admin-layout en todos los modulos de clientes-admin

--- cine-frontend/src/app/pages/admin/clientes-admin/inhabilitar-clientes/inhabilitar-clientes.component.ts
+++ cine-frontend/src/app/pages/admin/clientes-admin/inhabilitar-clientes/inhabilitar-clientes.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { NavbarAdminComponent } from '../../../../components/navbar-admin/navbar-admin.component';
-import { FooterComponent } from '../../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-inhabilitar-clientes',
-  imports: [NavbarAdminComponent, FooterComponent],
+  imports: [],
   templateUrl: './inhabilitar-clientes.component.html',
   styleUrl: './inhabilitar-clientes.component.css'
 })
