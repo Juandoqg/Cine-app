b0e3e67256777d4c449486e58759875774eddf63
Juandoqg <juandoqg@gmail.com>
2025-07-11 11:31:17 -0500

    navbar y footer para consultar e inhabilitar cliente en clientes-admin

--- cine-frontend/src/app/pages/admin/clientes-admin/consultar-clientes/consultar-clientes.component.ts
+++ cine-frontend/src/app/pages/admin/clientes-admin/consultar-clientes/consultar-clientes.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarAdminComponent } from '../../../../components/navbar-admin/navbar-admin.component';
+import { FooterComponent } from '../../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-consultar-clientes',
-  imports: [],
+  imports: [NavbarAdminComponent, FooterComponent],
   templateUrl: './consultar-clientes.component.html',
   styleUrl: './consultar-clientes.component.css'
 })
