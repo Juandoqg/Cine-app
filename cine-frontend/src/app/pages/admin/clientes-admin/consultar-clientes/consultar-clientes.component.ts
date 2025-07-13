bdbd82ac514a65f8551f9dcbdf86e902be4ea491
Juandoqg <juandoqg@gmail.com>
2025-07-12 22:07:23 -0500

    modal para inhabilitar

--- cine-frontend/src/app/pages/admin/clientes-admin/consultar-clientes/consultar-clientes.component.ts
+++ cine-frontend/src/app/pages/admin/clientes-admin/consultar-clientes/consultar-clientes.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../../../../models/usuario.model';
+import { UsuarioService } from '../../../../services/usuario.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-consultar-clientes',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './consultar-clientes.component.html',
-  styleUrl: './consultar-clientes.component.css'
+  styleUrls: ['./consultar-clientes.component.css']
 })
-export class ConsultarClientesComponent {
+export class ConsultarClientesComponent implements OnInit {
+  usuarios: Usuario[] = [];
 
+  mostrarModal = false;
+  usuarioSeleccionado: Usuario | null = null;
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.cargarUsuarios();
+  }
+
+  cargarUsuarios(): void {
+    this.usuarioService.obtenerUsuarios().subscribe({
+      next: (data) => {
+        this.usuarios = data.filter(u => u.rol === 'cliente');
+      },
+      error: (err) => console.error('Error cargando usuarios', err)
+    });
+  }
+
+  abrirModal(usuario: Usuario): void {
+    this.usuarioSeleccionado = usuario;
+    this.mostrarModal = true;
+  }
+
+  cerrarModal(): void {
+    this.mostrarModal = false;
+    this.usuarioSeleccionado = null;
+  }
+
+  confirmarInhabilitar(): void {
+    if (!this.usuarioSeleccionado) return;
+
+    this.usuarioService.inhabilitarUsuario(this.usuarioSeleccionado.id).subscribe({
+      next: () => {
+        this.usuarioSeleccionado!.activo = false;
+        this.cerrarModal();
+      },
+      error: (err) => {
+        console.error('Error al inhabilitar', err);
+        this.cerrarModal();
+      }
+    });
+  }
 }
---
+++ cine-frontend/src/app/services/usuario.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
+import { environment } from '../../environments/enviroment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  private readonly apiUrl = `${environment.apiUrl}/usuarios`;
+
+  constructor(private http: HttpClient) {}
+
+  obtenerUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(`${this.apiUrl}`);
+  }
+
+  inhabilitarUsuario(id: number): Observable<void> {
+    return this.http.patch<void>(`${this.apiUrl}/inhabilitar/${id}`, {});
+  }
+}
