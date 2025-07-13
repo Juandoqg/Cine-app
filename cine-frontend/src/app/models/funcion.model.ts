1c1265b0c7be056fe104cc8c792e9a99627cf2ed
Juandoqg <juandoqg@gmail.com>
2025-07-13 01:00:56 -0500

    crear funcion formulario realizado y estilos, falta funcionalidad

--- cine-frontend/src/app/models/funcion.model.ts
+++ cine-frontend/src/app/models/funcion.model.ts
@@ -5,4 +5,5 @@ export interface Funcion {
   salaId: string;
   peliculaId: string;
   precio:number
+  activo : boolean
 }
--- cine-frontend/src/app/pages/admin/funciones-admin/crear-funciones/crear-funciones.component.ts
+++ cine-frontend/src/app/pages/admin/funciones-admin/crear-funciones/crear-funciones.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { FuncionService } from '../../../../services/funcion.service';
+import { PeliculaService } from '../../../../services/pelicula.service';
+import { SalaService } from '../../../../services/sala.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-crear-funciones',
-  imports: [],
+  selector: 'app-crear-funcion',
+  imports: [CommonModule, FormsModule],
   templateUrl: './crear-funciones.component.html',
-  styleUrl: './crear-funciones.component.css'
+  styleUrls: ['./crear-funciones.component.css'],
 })
-export class CrearFuncionesComponent {
+export class CrearFuncionesComponent implements OnInit {
+  peliculas: any[] = [];
+  salas: any[] = [];
 
+  funcion = {
+    peliculaId: '',
+    salaId: '',
+    fecha: '',
+    hora: '',
+    precio: 0,
+    activo: true, // siempre true por defecto
+  };
+
+  constructor(
+    private funcionService: FuncionService,
+    private peliculaService: PeliculaService,
+    private salaService: SalaService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.peliculaService.getPeliculasActivas().subscribe(p => (this.peliculas = p));
+    this.salaService.obtenerTodas().subscribe(s => (this.salas = s));
+  }
+
+  onSubmit(): void {
+   
+  }
 }
