5c5247cc5b31216eeed3a00780677fa7b4db22de
Juandoqg <juandoqg@gmail.com>
2025-07-13 20:07:53 -0500

    guard por roles, proteccion de ventas controller

--- cine-backend/src/interfaces/controllers/venta.controller.ts
+++ cine-backend/src/interfaces/controllers/venta.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Post, Get , Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/infraestructure/guards/roles.guard';
+import { Roles } from 'src/infraestructure/decorators/roles.decorator';
 import { CrearVentaUseCase } from 'src/application/use-cases/ventas/crear-venta.use-case';
 import { ObtenerVentasUseCase } from 'src/application/use-cases/ventas/get-all-venta.use-case';
 import { GetVentasByClienteIdUseCase } from 'src/application/use-cases/ventas/get-venta-por-cliente-id.use-case';
@@ -7,24 +18,32 @@ import { VentaMapper } from 'src/infraestructure/mappers/venta.mapper';
 
 @Controller('ventas')
 export class VentaController {
-  constructor(private readonly crearVentaUseCase: CrearVentaUseCase,
+  constructor(
+    private readonly crearVentaUseCase: CrearVentaUseCase,
     private readonly obtenerVentas: ObtenerVentasUseCase,
-    private readonly getVentasByIdUseCase : GetVentasByClienteIdUseCase
+    private readonly getVentasByIdUseCase: GetVentasByClienteIdUseCase,
   ) {}
 
+  // 🟢 Crear venta - solo autenticado
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   async crearVenta(@Body() data: Omit<Venta, 'id'>): Promise<Venta> {
     return this.crearVentaUseCase.execute(data);
   }
 
-   @Get()
-   async obtenerTodas(): Promise<Venta[]> {
+  // 🔐 Ver todas las ventas - solo admin
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin')
+  @Get()
+  async obtenerTodas(): Promise<Venta[]> {
     const ventasOrm = await this.obtenerVentas.execute();
     return ventasOrm.map(VentaMapper.toDomain);
   }
 
-      @Get('cliente/:id')
-    async getVentasByCliente(@Param('id') id: string) {
-      return this.getVentasByIdUseCase .execute(id);
-    }
+  // 🟢 Ver ventas de un cliente - autenticado
+  @UseGuards(AuthGuard('jwt'))
+  @Get('cliente/:id')
+  async getVentasByCliente(@Param('id') id: string) {
+    return this.getVentasByIdUseCase.execute(id);
+  }
 }
